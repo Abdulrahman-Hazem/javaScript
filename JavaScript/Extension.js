502ee5834e8fbfbe1846365e81bf22220c70df4d
Abdulrahman-Hazem <96590025+Abdulrahman-Hazem@users.noreply.github.com>
2024-08-11 14:12:16 +0300

    Add files via upload

---
+++ JavaScript/Extension.js
@@ -0,0 +1,10 @@
+// Define a constant
+const greeting = "Greetings World!";
+
+// Define a function
+const hello = () => {
+  return "Hello World!";
+};
+module.exports = { hello, greeting };
+
+/// { Import } => Basics.js => Line 428
